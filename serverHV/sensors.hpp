236798057243a3002cf8e93210349cb3a7270d33
Adrien <adrien.buot@gmail.com>
2021-01-25 11:28:31 +0100

    erased errors

--- serverHV/sensors.hpp
+++ serverHV/sensors.hpp
@@ -17,41 +17,7 @@ private:
 protected:
     int address;
     int pin;
-    typeState_t state;
-
-    void setState(typeState_t state);
-
-public:
-    Sensor(int id, int pin, int address);
-    void init();
-    void setClient(WiFiClient client);
-    void readSensor();
-};
-
-class SensorShort : public Sensor{
-private:
-    Adafruit_VL53L0X lox = Adafruit_VL53L0X();
-
-public:#include <WiFi.h> 
-#include "Adafruit_VL53L0X.h"
-typedef enum SensorState{
-    IDLE = 0,
-    LOWENERGY = 1,
-    MEDIUMENERGY = 2,
-    HIGHENERGY = 3
-}typeState_t;
-
-class Sensor{
-private:
-    WiFiClient client;
-    int id;
-
-    void sendToActuator();
-
-protected:
-    int address;
-    int pin;
-    typeState_t state;
+    typeState_t state = IDLE;
 
     void setState(typeState_t state);
 
@@ -81,35 +47,6 @@ public:
 
 class SensorBottom : public SensorShort{
 public:
-    SensorBottom(int id, int pin, int address);
-    void readSensor();
-};
-
-class SensorTop : public SensorShort{
-public:
-    SensorTop(int id, int pin, int address);
-    void readSensor();
-};
-    SensorShort(int id, int pin, int address);
-    void init();
-    void readSensor();
-};
-
-class SensorLong : public Sensor{
-public:
-    SensorLong(int id, int pin, int address);
-    void init();
-    void readSensor();
-};
-
-class SensorBottom : public SensorShort{
-public:
-    SensorBottom(int id, int pin, int address);
-    void readSensor();
-};
-
-class SensorTop : public SensorShort{
-public:
-    SensorTop(int id, int pin, int address);
+    SensorBottom(int id, int pin, int address) : SensorShort(id, pin, address){}
     void readSensor();
 };
