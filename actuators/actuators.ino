4c4988aabb122aaa4409fabd3a332ff3107d41ef
Adrien <adrien.buot@gmail.com>
2021-02-01 15:47:15 +0100

    documentation

--- actuators/actuators.ino
+++ actuators/actuators.ino
@@ -3,52 +3,64 @@
 #define NB_ACTUATORS 1
 #define PIN 27
 #define NB_CLIENTS 1
+#define ACTUATOR_ID 0
+#define INTENSITY 1
 
 const char *ssid = "Server Movuino";     
-const char *password = "Adrienestsib0";
+const char *password = "P@ssw0rd";
 WiFiServer wifiServer(80, NB_CLIENTS);
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NB_ACTUATORS, PIN, NEO_GRB + NEO_KHZ800);
 /**
- * @brief 
+ * @brief Setup of the microcontroller
+ * 
+ * We initiate the actuators to low before all, then we try to connect to the hotspot wifi, and set up the server
  * 
  */
 void setup() {
-  Serial.begin(115200);    
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED);
-  Serial.println(WiFi.localIP());
-  wifiServer.begin();
+  Serial.begin(115200);  
   pixels.begin();
+  //We set the actuators to low
   for(int i = 0; i< NB_ACTUATORS; i++){
     pixels.setPixelColor(i, pixels.Color(255,0,0));
   }
-  pixels.show();
+  //We send the data to actuators
+  pixels.show();  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED);
+  Serial.println(WiFi.localIP()); //The IP address should be 192.168.4.2
+  wifiServer.begin();
 }
 
 /**
- * @brief 
+ * @brief loop 
+ * 
+ * We get a client if there is any, if there is no client avaible, the loop is finished
+ * When a client is connected, we enter in a loop while there is a client
+ * When a client is available, we receive data from the client and do the actions
  * 
  */
 void loop() {
-  uint8_t i, intensity;
   WiFiClient client = wifiServer.available();
-    if(client){
-      while (client.connected()) {
-        while (client.available()>0) {
-          uint8_t line[2];
-          client.read(line, 2);
-          i = line[0];
-          intensity = line[1];
-          Serial.print(i);
-          Serial.println(intensity);
-          if(i < NB_ACTUATORS){
-            pixels.setPixelColor(i, pixels.Color(intensity,0,0));
-            pixels.show();
-          }
-          delay(10);
+  if(client){
+    while (client.connected()) {
+      while (client.available()>0) {
+        //We get and print the data received
+        uint8_t line[2];
+        client.read(line, 2);
+        Serial.print("actuator id : ");
+        Serial.print(line[ACTUATOR_ID]);
+        Serial.print(" intensity : ");
+        Serial.println(line[INTENSITY]);
+        //If there is an actuator related to the sensor
+        if(line[ACTUATOR_ID] < NB_ACTUATORS){
+          //We change the state of the actuator
+          pixels.setPixelColor(line[ACTUATOR_ID], pixels.Color(line[INTENSITY],0,0));
+          pixels.show();
         }
+        delay(10);
       }
-      client.stop();
     }
+    client.stop();
+  }
 }
--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -1,22 +1,25 @@
 #include "sensors.hpp"
 #define NB_SENSORS 2
 
-// Set these to your desired credentials.
-const char *Apssid = "Server Movuino";     //Give AccessPoint name whatever you like. (this will be Name of your esp32 HOTSPOT)
-const char *Appassword = "Adrienestsib0";         //Password of your Esp32's hotspot,(minimum length 8 required)
-const char * host = "192.168.4.2";
-const uint16_t port = 80;
+const char *Apssid = "Server Movuino"; //Name of the hotspot wifi
+const char *Appassword = "P@ssw0rd"; //password
+const char * host = "192.168.4.2"; //Ip address of the server
+const uint16_t port = 80; //port of the server
 
 
 Sensor *sensors[NB_SENSORS];
-
+/**
+ * @brief Initialisation of sensors
+ * 
+ * We create a hotspot wifi and instanciate all sensors
+ * 
+ */
 void setup() {
-  Serial.begin(115200);                   // To enable Serial Commmunication with connected esp32 board
+  Serial.begin(115200); 
   Serial.println("Configuring access point...");
-  WiFi.mode(WIFI_AP);                    // Changing ESP32 wifi mode to AccessPoint
+  WiFi.mode(WIFI_AP); //ESP32 can now be a hotspot wifi
 
-  // You can remove the Appassword parameter if you want the hotspot to be open.
-  WiFi.softAP(Apssid, Appassword);      //Starting AccessPoint on given credential
+  WiFi.softAP(Apssid, Appassword); //We create the hotspot wifi
 
   /* 
     We initialize all sensors 
@@ -28,23 +31,34 @@ void setup() {
     sensors[i]->init();
 }
 
+/**
+ * @brief loop
+ * 
+ * We try to connect to the server (the actuators)
+ * When we found it, we give the information to sensors and begin getting data for each sensors
+ * 
+ */
 void loop() {
   WiFiClient client;
+  // We try to connect to the server
   if (!client.connect(host, port)) {
     Serial.println("Connection to host failed");
     delay(1000);
     return;
   }
+  //If we are connected to the server
   if (client) {
+    //We give the client to each sensors to let them send information
     for(int i = 0; i < NB_SENSORS; i++)
       sensors[i]->setClient(client);
+    //While we are connected to the server we get the data of the sensors 
     while (client.connected()) {
       for(int i = 0; i < NB_SENSORS; i++)
         sensors[i]->readSensor();
  
       delay(100);
     }
- 
+  
     client.stop();
     Serial.println("Client disconnected");
  
