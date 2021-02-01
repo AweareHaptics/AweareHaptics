d792685cb9231ef9565b912f2d5295c014295ca9
Adrien <adrien.buot@gmail.com>
2021-02-01 11:33:15 +0100

    rename

---
+++ actuators/actuators.ino
@@ -0,0 +1,54 @@
+#include <WiFi.h>
+#include <Adafruit_NeoPixel.h>
+#define NB_ACTUATORS 1
+#define PIN 27
+#define NB_CLIENTS 1
+
+const char *ssid = "Server Movuino";     
+const char *password = "Adrienestsib0";
+WiFiServer wifiServer(80, NB_CLIENTS);
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NB_ACTUATORS, PIN, NEO_GRB + NEO_KHZ800);
+/**
+ * @brief 
+ * 
+ */
+void setup() {
+  Serial.begin(115200);    
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED);
+  Serial.println(WiFi.localIP());
+  wifiServer.begin();
+  pixels.begin();
+  for(int i = 0; i< NB_ACTUATORS; i++){
+    pixels.setPixelColor(i, pixels.Color(255,0,0));
+  }
+  pixels.show();
+}
+
+/**
+ * @brief 
+ * 
+ */
+void loop() {
+  uint8_t i, intensity;
+  WiFiClient client = wifiServer.available();
+    if(client){
+      while (client.connected()) {
+        while (client.available()>0) {
+          uint8_t line[2];
+          client.read(line, 2);
+          i = line[0];
+          intensity = line[1];
+          Serial.print(i);
+          Serial.println(intensity);
+          if(i < NB_ACTUATORS){
+            pixels.setPixelColor(i, pixels.Color(intensity,0,0));
+            pixels.show();
+          }
+          delay(10);
+        }
+      }
+      client.stop();
+    }
+}
---
+++ sensors/sensors.ino
@@ -0,0 +1,53 @@
+#include "sensors.hpp"
+#define NB_SENSORS 2
+
+// Set these to your desired credentials.
+const char *Apssid = "Server Movuino";     //Give AccessPoint name whatever you like. (this will be Name of your esp32 HOTSPOT)
+const char *Appassword = "Adrienestsib0";         //Password of your Esp32's hotspot,(minimum length 8 required)
+const char * host = "192.168.4.2";
+const uint16_t port = 80;
+
+
+Sensor *sensors[NB_SENSORS];
+
+void setup() {
+  Serial.begin(115200);                   // To enable Serial Commmunication with connected esp32 board
+  Serial.println("Configuring access point...");
+  WiFi.mode(WIFI_AP);                    // Changing ESP32 wifi mode to AccessPoint
+
+  // You can remove the Appassword parameter if you want the hotspot to be open.
+  WiFi.softAP(Apssid, Appassword);      //Starting AccessPoint on given credential
+
+  /* 
+    We initialize all sensors 
+  */
+  sensors[0] = new SensorLong(0,27,0x30);
+  sensors[1] = new SensorShort(1,14,0x31);
+  //sensors[2] = new SensorLong(2,15, 0x32);
+  for(int i = 0; i < NB_SENSORS; i++)
+    sensors[i]->init();
+}
+
+void loop() {
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    Serial.println("Connection to host failed");
+    delay(1000);
+    return;
+  }
+  if (client) {
+    for(int i = 0; i < NB_SENSORS; i++)
+      sensors[i]->setClient(client);
+    while (client.connected()) {
+      for(int i = 0; i < NB_SENSORS; i++)
+        sensors[i]->readSensor();
+ 
+      delay(100);
+    }
+ 
+    client.stop();
+    Serial.println("Client disconnected");
+ 
+  }
+    delay(100);
+}
